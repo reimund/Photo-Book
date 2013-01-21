e1cdaf1ed6c70a9eab2728c17e6617ce81df588a
Reimund Trost <reimund@code7.se>
2013-01-21 18:55:05 +0100

    Pages should not turn through each other anymore.

--- jquery.photobook.js
+++ jquery.photobook.js
@@ -22,11 +22,23 @@ var PREVIOUS = -1;
 		}, options);
 
 		self = this;
+
+		self.pages = [];
+
+		// Used to prevent rotation of pages in both directions.
 		self.is_turning_forwards = false;
 		self.is_turning_backwards = false;
+
+		// Horizontal distance from book center. Used to calculate how far one
+		// have to drag to get a full turn.
 		self.drag_start = null;
-		self.drag_speed = 1;
+
+		// Used to calculate drag speed.
 		self.last_mouse_x = -1;
+		self.drag_speed = 1;
+
+		// Used to prevent pages to rotate through each other.
+		self.current_max_turn = 0;
 
 
 		this.init = function()
@@ -49,12 +61,6 @@ var PREVIOUS = -1;
 			$(self).width(self.width).height(self.height);
 			
 			self.left_page = $('<div/>', { 'class': 'left_page'});
-			
-			// The element that will act as a flipping page.
-			self.page = $('<div/>', { 'class': 'page' });
-
-			self.turning_page_front = $('<div/>', { 'class': 'turning_page_front'}).appendTo(self.page);
-			self.turning_page_back = $('<div/>', { 'class': 'turning_page_back'}).appendTo(self.page);
 			self.left_page.prependTo(self);
 			self.images.remove();
 			self.css('background-image', 'url('+ $(self.images.get(self.current_image)).attr('src') + ')');
@@ -65,17 +71,22 @@ var PREVIOUS = -1;
 
 		this.next = function()
 		{
-			self.turn(NEXT);
-			self.turn_animation(self.find('.page').first());
+			var page = self.prepare_for_turn(NEXT)
+
+			if (page)
+				self.turn_animation(page);
 		};
 
 		this.previous = function()
 		{
-			self.turn(PREVIOUS);
-			self.turn_animation(self.find('.page').first());
+			var page = self.prepare_for_turn(PREVIOUS);
+
+			if (page)
+				self.turn_animation(page);
+
 		};
 		
-		this.turn = function(direction)
+		this.prepare_for_turn = function(direction)
 		{
 			var turning_page, next_image, rot_y;
 
@@ -85,8 +96,7 @@ var PREVIOUS = -1;
 					return;
 				
 				self.is_turning_forwards = true;
-				r0 = 0; // Start rotation.
-				r1 = -180; // End rotation.
+				target_y = 0; // Start rotation.
 				left_image = self.current_image_left;
 				right_image = (self.current_image + 1).mod(self.images.length);
 				turning_page_front_image = self.current_image;
@@ -97,43 +107,75 @@ var PREVIOUS = -1;
 					return;
 
 				self.is_turning_backwards = true;
-				r0 = -180;
-				r1 = 0;
+				target_y = -180;
 				left_image = (self.current_image - 1).mod(self.images.length);
 				right_image = self.current_image_right;
 				turning_page_front_image = left_image;
 				turning_page_back_image = self.current_image;
 			}
 
-			turning_page = self.page.clone();
-			self.left_page.after(turning_page);
+			var turning_page = new TurningPage(self);
+
+			self.pages.push(turning_page);
+			self.left_page.after(turning_page.el);
 			self.drag_speed = 1;
 
-			// Left.
+			// Set backgrounds on left & right pages.
 			self.left_page.set_bg(self.get_image(left_image));
-
-			// Right.
 			self.set_bg(self.get_image(right_image));
 
 			// Turning page, NEXT => front, PREVIOUS => back.
-			turning_page.find('.turning_page_front').set_bg(self.get_image(turning_page_front_image));
+			turning_page.front.set_bg(self.get_image(turning_page_front_image));
 			
 			// Turning page, NEXT => back, PREVIOUS => front.
-			turning_page.find('.turning_page_back').set_bg(self.get_image(turning_page_back_image));
+			turning_page.back.set_bg(self.get_image(turning_page_back_image));
 
-			turning_page.data('reordered', 'no');
-			turning_page.data('direction', direction);
-			turning_page.data('index', self.current_image);
-			turning_page.rotate_y(r0);
+			turning_page.reordered = false;
+			turning_page.direction = direction;
+			turning_page.image_index = self.current_image;
+			turning_page.rotate_y(target_y);
 
-			if (NEXT == direction)
+			if (NEXT == direction) {
 				self.current_image = (self.current_image + 1).mod(self.images.length);
-			else
+			} else {
 				self.current_image = (self.current_image - 1).mod(self.images.length);
+			}
+			
+			return turning_page;
+		};
 
-			// Turn the page.
+		this.rotation_bounds = function(direction, page)
+		{
+			var i;
+
+			// Find out what index the specified page currently has in the
+			// pages array (stored in variable i).
+			for (i in self.pages) {
+				if (self.pages[i].y == page.y)
+					break;
+			}
+
+			if (i > 0)
+				if (NEXT == direction)
+					return [self.pages[i - 1].y + 0.25, 0];
+				else
+					return [-180, self.pages[i - 1].y - 0.25]
+			else
+				return [-180, 0];
+		}
+
+		this.drag_turn = function(direction)
+		{
+			// Will return null if an ongoing turn is already taking place in
+			// the opposite direction.
+			var turning_page = self.prepare_for_turn(direction)
+
+			if (!turning_page)
+				return;
+
+			// Turn the page by moving the mouse.
 			self.on('vmousemove', function(e) {
-				var current_drag, y, mouse_x;
+				var current_drag, y, mouse_x, min_max;
 
 				current_drag = (e.pageX - this.offsetLeft) - self.width / 2;
 				y = Math.min(0, Math.max(-180, (current_drag - self.drag_start) * (180 / (self.drag_start * 2))));
@@ -142,50 +184,29 @@ var PREVIOUS = -1;
 				if (PREVIOUS == direction)
 					y = -180 -y;
 
+				// Make sure we don't rotate the current page past pages that
+				// are already turning.
+				min_max = self.rotation_bounds(direction, turning_page);
+				y = Math.max(min_max[0], y);
+				y = Math.min(min_max[1], y);
+
 				// Capture mouse speed while dragging.
 				if (-1 < self.last_mouse_x)
 					self.drag_speed = self.last_mouse_x - mouse_x;
-					
+
 				self.last_mouse_x = mouse_x;
 
 				turning_page.rotate_y(y);
 			});
+		}
 
-		};
-
-		this.turn_animation = function(turning_page)
+		this.turn_animation = function(page)
 		{
-			var direction, current_y, target_y, duration;
-
-			direction = turning_page.data('direction');
-			current_y = self.get_rotation_degrees(turning_page);
-			target_y = NEXT == direction ? -180 : 0;
-
-			// Make the duration shorter the further the page has been dragged.
-			duration = Math.abs(current_y-target_y) / 180 * settings.page_flip_duration;
-
-			// Make it even shorter depending on the current drag speed.
-			duration = duration / Math.max(1, Math.log(Math.abs(self.drag_speed * 0.5)));
-			turning_page.css('textIndent', current_y);
-			turning_page.animate({textIndent: target_y}, {
-				step: function(now, fx) {
-					// When the pages have turned more than 90 degrees we must
-					// flip the ordering of the pages for it to look right.
-					if (((NEXT == direction && -90 > now)
-							|| (PREVIOUS == direction && -90 < now)
-							) && $(this).data('reordered') == 'no') {
-						var parent = $(this).parent();
-						$(this).detach();
-						parent.append($(this));
-						$(this).data('reordered', 'yes');
-					}
-
-					$(this).rotate_y(now);
-				},
-				duration: duration,
-				easing: 'easeInOutCubic',
+			page.animate_turn({
+				drag_speed: self.drag_speed,
+				duration: settings.page_flip_duration,
 				complete: function() {
-					if (NEXT == direction) {
+					if (NEXT == page.direction) {
 						 self.current_image_left = (self.current_image_left + 1).mod(self.images.length);
 						 self.current_image_right = (self.current_image_right + 1).mod(self.images.length);
 						 right_image = self.current_image;
@@ -193,46 +214,30 @@ var PREVIOUS = -1;
 					} else {
 						 self.current_image_left = (self.current_image_left - 1).mod(self.images.length);
 						 self.current_image_right = (self.current_image_right - 1).mod(self.images.length);
-						 right_image = $(this).data('index') - 1;
+						 right_image = page.image_index - 1;
 						 left_image = self.current_image;
 					}
 					
-					$(this).remove();
+					page.el.remove();
 
 					if (self.find('.page').length < 1)
-						if (NEXT == direction)
+						if (NEXT == page.direction)
 							self.is_turning_forwards = false;
 						else
 							self.is_turning_backwards = false;
 
 					self.left_page.set_bg(self.get_image(left_image));
 					self.set_bg(self.get_image(right_image));
+
+					// Reset max turn.
+					//self.current_max_turn = 0;
+					self.pages.shift();
 				}
 			});
 		};
 
 		this.setup_page_buttons = function()
 		{
-			$('body').on('vmouseup', function(e) {
-				if (e.which != 1 && e.which != 0)
-					return;
-
-				var parent_offset, rel_x, page;
-
-				parent_offset = $(this).parent().offset(); 
-				rel_x = e.pageX - parent_offset.left;
-				page = self.find('.page');
-
-				// Stop dragging.
-				self.unbind('vmousemove');
-				self.drag_start = null;
-
-				if (page.length > 0)
-					self.turn_animation(page.first());
-
-				e.preventDefault();
-			});
-
 			self.on('vmousedown', function(e) {
 				if (e.which != 1 && e.which != 0)
 					return;
@@ -243,34 +248,37 @@ var PREVIOUS = -1;
 				self.drag_start = (e.pageX - this.offsetLeft) - self.width / 2;
 
 				if ((rel_x / $(this).width()) > 0.5)
-					self.turn(NEXT);
+					self.drag_turn(NEXT);
 				else
-					self.turn(PREVIOUS);
+					self.drag_turn(PREVIOUS);
 				
 				e.preventDefault();
 			});
+
+			$('body').on('vmouseup', function(e) {
+				if (e.which != 1 && e.which != 0)
+					return;
+
+				var page, parent_offset, rel_x;
+
+				page = self.pages[self.pages.length - 1];
+
+				if (!page || null == self.drag_start)
+					return;
+
+				parent_offset = $(this).parent().offset(); 
+				rel_x = e.pageX - parent_offset.left;
+
+				// Stop dragging.
+				self.unbind('vmousemove');
+				self.drag_start = null;
+				self.turn_animation(page);
+
+				e.preventDefault();
+			});
+
 		};
 
-		this.get_rotation_degrees = function(el)
-		{
-			var matrix = el.css("-webkit-transform")
-				|| el.css("-moz-transform")
-				|| el.css("-ms-transform")
-				|| el.css("-o-transform")
-				|| el.css("transform");
-
-			if (matrix !== 'none') {
-				var values = matrix.split('(')[1].split(')')[0].split(',');
-				var a = values[0];
-				var b = values[2];
-				var angle = -Math.atan2(b, a) * (180/Math.PI);
-			} else {
-				var angle = 0;
-			}
-
-			return angle;
-		};
-		
 		/* Gets the src of the image of the specified index. */
 		this.get_image = function(index)
 		{
@@ -296,9 +304,103 @@ var PREVIOUS = -1;
 }(jQuery));
 
 
+var x = 0;
+var TurningPage = function(book)
+{
+	this.name = 'Page ' + x++;
+	this.el = $('<div/>', { 'class': 'page' });
+	this.front = $('<div/>', { 'class': 'front'}).appendTo(this.el);
+	this.back = $('<div/>', { 'class': 'back'}).appendTo(this.el);
+	this.y = 0;
+	this.book = book;
 
-/* Modulus implementation that handles negative numbers nicer than JavaScript's
- * remainder operator (%). */
+	this.reordered = 'no';
+	this.direction = null;
+	this.image_index = null;
+
+	this.rotate_y = function(y)
+	{
+		this.el.rotate_y(y);	
+		this.y = y;
+		this.backface_visibility_fix();
+
+		// XXX: This doesn't really belong here, it's something that should be
+		// performed by the book.
+		//
+		// When the pages have turned more than 90 degrees we must
+		// flip the ordering of the pages for it to look right.
+		if (((NEXT == this.direction && -90 > y)
+				|| (PREVIOUS == this.direction && -90 < y)
+				) && !this.reordered) {
+
+			var parent = this.el.parent();
+			this.el.detach();
+			parent.append(this.el);
+			this.reordered = true;
+		}
+	};
+
+	this.animate_turn = function(options)
+	{
+		var self, target_y, duration, easing;
+
+		self = this;
+		target_y = NEXT == self.direction ? -180 : 0;
+
+		// Don't ease in when released from drag.
+		easing = Math.round(self.y) == 0 || Math.round(self.y) == -180
+					? 'easeInOutCubic' : 'easeOutCubic';
+
+		// Make the duration shorter the further the page has been dragged.
+		duration = Math.abs(self.y - target_y) / 180 * options.duration;
+
+		// Make it even shorter depending on the current drag speed.
+		duration = duration / Math.max(1, Math.log(Math.abs(options.drag_speed * 0.5)));
+
+		duration = 8000;
+
+		self.el.css('textIndent', self.y);
+		self.el.animate({textIndent: target_y}, {
+			step: function(y, fx) {
+				var min_max = self.book.rotation_bounds(self.direction, self);
+
+				// Prevent the current page from turning through pages that
+				// already are turning.
+				y = Math.max(min_max[0], y);
+				y = Math.min(min_max[1], y);
+
+				self.rotate_y(y);
+			},
+			duration: duration,
+			easing: easing,
+			complete: options.complete,
+		});
+	}
+
+	/**
+	  * Flip visibility. We need to do this since IE doesn't
+	  * fully support backface-visiblity.
+	  */
+	this.backface_visibility_fix = function()
+	{
+		if (-90 > this.y) {
+			if (this.front.css('visibility') == 'visible') {
+				this.front.css('visibility', 'hidden');
+				this.back.css('visibility', 'visible');
+			}
+		} else if (-90 <= this.y) {
+			if (this.front.css('visibility') == 'hidden') {
+				this.front.css('visibility', 'visible');
+				this.back.css('visibility', 'hidden');
+			}
+		}
+	};
+}
+
+/**
+ * Modulus implementation that handles negative numbers nicer than JavaScript's
+ * remainder operator (%).
+ */
 Number.prototype.mod = function(n)
 {
 	return ((this % n) + n) % n;
